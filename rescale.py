a38beb5576e76d2be6288b234f68107cfb5f8647
SenithSV <ssvsenith@gmail.com>
2025-01-11 14:52:42 +0530

    Add functions to rescale frames of videos and images

--- rescale.py
+++ rescale.py
@@ -1,1 +1,33 @@
 import cv2 as cv
+
+img = cv.imread('Resources/Photos/cat.jpg')
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimenstions = (width, height)
+
+    return cv.resize(frame, dimenstions, interpolation=cv.INTER_AREA)
+
+# reading images
+
+
+resized_img = rescaleFrame(img)
+cv.imshow('Cat', resized_img)
+
+# reading videos
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame)
+    # cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()  
+
+cv.waitKey(0)
